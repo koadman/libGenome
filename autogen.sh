6edf846a27b3d42f48aa7bf5bd2f3291062566c4
Aaron Darling <aaron.darling@uts.edu.au>
2006-11-15 10:45:47 +0000

    refined messages

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 (libtoolize || glibtoolize) && \
 aclocal && \
+echo "Ignore any messages about adding aclocal.m4 && \
 autoheader && \
 automake -a && \
 autoconf && \
-echo "Now run ./configure --prefix=$HOME ; make install" && \
+echo "Now run ./configure --prefix=$HOME && make install" && \
 echo "Add --disable-shared to the configure line if building on Mac OS X"
 
