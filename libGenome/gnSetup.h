f855d860794137d93f5be6d6462f382ea5fba5d9
Aaron Darling <aaron.darling@uts.edu.au>
2007-11-08 08:47:53 +0000

    library auto-link for windows

--- libGenome/gnSetup.h
+++ libGenome/gnSetup.h
@@ -30,5 +30,32 @@
 #define GNDLLEXPORT_DATA
 #endif
 
+// attempt to auto-link the genome library on windows
+#if defined(WIN64)&&defined(NDEBUG)&&defined(_OPENMP)
+#pragma comment(lib, "genome64omp.lib")
+#endif
+#if defined(WIN64)&&!defined(NDEBUG)&&defined(_OPENMP)
+#pragma comment(lib, "genome64fdomp.lib")
+#endif
+#if defined(WIN32)&&!defined(WIN64)&&defined(NDEBUG)&&defined(_OPENMP)
+#pragma comment(lib, "genomeomp.lib")
+#endif
+#if defined(WIN32)&&!defined(WIN64)&&!defined(NDEBUG)&&defined(_OPENMP)
+#pragma comment(lib, "genomefdomp.lib")
+#endif
+#if defined(WIN64)&&defined(NDEBUG)&&!defined(_OPENMP)
+#pragma comment(lib, "genome64.lib")
+#endif
+#if defined(WIN64)&&!defined(NDEBUG)&&!defined(_OPENMP)
+#pragma comment(lib, "genome64fd.lib")
+#endif
+#if defined(WIN32)&&!defined(WIN64)&&defined(NDEBUG)&&!defined(_OPENMP)
+#pragma comment(lib, "genome.lib")
+#endif
+#if defined(WIN32)&&!defined(WIN64)&&!defined(NDEBUG)&&!defined(_OPENMP)
+#pragma comment(lib, "genomefd.lib")
+#endif
+
+
 #endif
 	//_gnSetup_h_
