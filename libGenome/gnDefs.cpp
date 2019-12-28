b78de0bf502ef6251b57c1ef59b6e751310c88e2
Aaron Darling <aaron.darling@uts.edu.au>
2019-12-28 22:31:44 +0000

    [AD] conditional compile required for newer gcc

--- libGenome/gnDefs.cpp
+++ libGenome/gnDefs.cpp
@@ -5,7 +5,8 @@
 #include "libGenome/gnDefs.h"
 
 // some compilers don't have abs() for 64 bit ints
+#if (defined(__GNUG__) && ( __GNUC__ <= 2 )) || defined(__INTEL_COMPILER) || (defined _MSC_VER && defined __cplusplus)
 int64 abs( int64 a ){
 	return a < 0 ? -a : a;
 }
-
+#endif
